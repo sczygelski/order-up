1970a1dc5596262896889d779ef537d1d29876d9
Jonathan Thompson <jathompson399@gmail.com>
2022-08-31 21:40:31 -0500

    idk why its making me commit

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const path = require('path');
 const express = require('express');
-// const session = require('express-session');
-// const exphbs = require('express-handlebars')
+const session = require('express-session');
+const exphbs = require('express-handlebars')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
 
-//app.engine('handlebars', hbs.engine);
-//app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: false}));
-//app.use(express.static(path.join(_dirname, 'public')));
-//
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(_dirname, 'public')));
+
 sequelize.sync({ force: false});
