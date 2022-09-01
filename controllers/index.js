ce088f3307b48e8db03b7f4f3d2f7fd413220eda
Jonathan Thompson <jathompson399@gmail.com>
2022-08-31 20:43:20 -0500

    working on handlebars stuff

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const homeRoutes = require('./home-routes');
+const router = require('express').Router();
+// const apiRoutes = require('./api');
+
+// router.use('/api', apiRoutes);
+
+router.use('/', homeRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
