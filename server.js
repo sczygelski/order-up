ed9380b3c700280e987789b62259a68833391468
Sczygelski <avamariescz@gmail.com>
2022-08-31 20:27:56 -0500

    server.js updates

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars')
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(express.static(path.join(_direname, 'public')));
+
+sequelize.sync({ force: false});
