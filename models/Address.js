1ede5b49a23a0184119bd2477eb58c011ce02de9
Kal O <koestreicher4@gmail.com>
2022-09-01 17:17:10 -0500

    working User and Address Models

---
+++ models/Address.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+// create our Address Model
+class Address extends Model {}
+
+
+// Creating fileds/columns for Address model
+
+Address.init(
+    {
+
+        // ID of the address
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // House number
+        houseNumber: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        // Street address
+        street: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // City
+        city: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // State
+        state: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }
+);
+
+module.exports = Address;
--- models/User.js
+++ models/User.js
@@ -6,25 +6,39 @@ class User extends Model { }
 User.init(
     {
         // what fields should we require?
+
+
+        // the ID column, auto Increments
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true
         },
-        login: {
+
+        // Username Column
+        username: {
             type: DataTypes.STRING,
             allowNull: false
 
         },
+        // Email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+
+        // Password column
         password: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [6]
+                len: [4]
             }
         }
-
     },
 
     {
