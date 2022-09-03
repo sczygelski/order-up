1a86743535d91976e7ee83d5f63ef3fc2f41fafc
Susan Brown <brown684408@gmail.com>
2022-09-03 15:35:16 -0500

    work on login and logout routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-// Uncomment these below when routes.js files are setup
-// const addressRoutes = require('./address-routes');
-// const reviewRoutes = require('./review-routes');
+const addressRoutes = require('./address-routes');
+const reviewRoutes = require('./review-routes');
 
 router.use('/users', userRoutes);
-// Uncomment these below when routes.js files are setup
-// router.use('/addresses', addressRoutes);
-// router.use('/reviews', reviewRoutes);
+router.use('/addresses', addressRoutes);
+router.use('/reviews', reviewRoutes);
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,6 +6,14 @@ router.get("/", (req, res) => {
   res.render("homepage");
 });
 
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
 router.get('/', (req, res) => {
   res.render('homepage', {
     id: 1,
