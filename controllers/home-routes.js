b1f67f192e1757fac6d85afffec9751ad2dbd8b7
Jonathan Thompson <jathompson399@gmail.com>
2022-09-05 16:59:14 -0500

    asdf

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,11 +2,10 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { User, Address } = require("../models");
 
-// router.get("/", (req, res) => {
-//   res.render("homepage");
-// });
 
-
+router.get("/", (req, res) => {
+  res.render("homepage");
+});
 
 router.get('/', (req, res) => {
   res.render('homepage', {
@@ -22,13 +21,12 @@ router.get('/', (req, res) => {
   });
 });
 
-
 router.get('/login', (req, res) => {
-  // if (req.session.loggedIn) {
-  //   res.redirect('/');
-  //   return;
-  // }
-  res.render('/api/users/login');
+  res.render('login');
+});
+
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard');
 });
 
 module.exports = router;
