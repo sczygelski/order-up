2f021f4a2c02f0ce39618f504f7050ce2847c9e0
Kal O <koestreicher4@gmail.com>
2022-09-04 21:25:02 -0500

    login route fix

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,14 +17,12 @@ router.get('/', (req, res) => {
 });
 
 
-// router.get('/login', (req, res) => {
-//   if (req.session.loggedIn) {
-//     res.redirect('/');
-//     return;
-//   }
-
-//   res.render('login');
-  
-// });
+router.get('/login', (req, res) => {
+// if (req.session.loggedIn) {
+//   res.redirect('/');
+//   return;
+// }
+res.render('login');
+});
 
 module.exports = router;
