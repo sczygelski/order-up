6c49cf1d432a84b3455bea82092ff7bcb8a45cc9
Sczygelski <avamariescz@gmail.com>
2022-09-05 16:57:21 -0500

    fixing routes

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const path = require("path");
 const express = require("express");
-//const session = require('express-session');
+const session = require("express-session");
 const exphbs = require("express-handlebars");
-
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const sequelize = require("./config/connection");
-//const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-//app.use(session(sess));
+app.use(session(sess));
 
-const helpers = require('./utils/helpers');
+const helpers = require("./utils/helpers");
 
 const hbs = exphbs.create({});
 
@@ -33,11 +32,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-
 app.use(require("./controllers/"));
-
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
-
