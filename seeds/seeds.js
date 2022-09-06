5fc8cdcbf980ad91bccdfab6868ab344a437967c
Sczygelski <avamariescz@gmail.com>
2022-09-05 20:33:41 -0500

    seeds

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,11 +1,21 @@
 const sequelize = require("../config/connection");
 import { faker } from "@faker-js/faker";
 
-const { User } = require("../models");
+const { User, Address, Review } = require("../models");
 
 const randomUsername = faker.name.lastName(); // Nikolaus
 const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
 const setpassword = "password"
+const randomhouseNumber = faker.address.randombuildingNumber();
+const randomStreet = faker.address.street();
+const randomcity = faker.address.city();
+const randomstate = faker.address.state();
+const randomrating = faker.datatype.bigInt({max: 5});
+const randomaddress = faker.address.streetAddress();
+const randomreviewbody = faker.lorem.paragraph(4);
+const randomexcerpt = faker.lorem.sentence();
+
+
 
 
 
@@ -14,13 +24,44 @@ const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
     for (let i = 0; i < 10; i++) {
-        User.create({
-          "id": id,
-          "username": randomUsername,
-          "email": randomEmail,
-          "password": setpassword
-        })
+      User.create({
+        id: id,
+        username: randomUsername,
+        email: randomEmail,
+        password: setpassword
+      })
     }
+
+    for (let i = 0; i < 10; i++) {
+      Address.create({
+        id: id,
+        houseNumber: randomhouseNumber,
+        street: randomStreet,
+        city: randomcity,
+        state: randomstate
+      })
+      
+    }
+
+    for (let i = 0; i < 10; i++) {
+      Review.create({
+        id: id,
+        rating: 
+      })      
+    }
+
+    for (let i = 0; i < 10; i++) {
+      Address.create({
+        id: id,
+        rating: randomrating,
+        address: randomaddress,
+        reviewbody: randomreviewbody,
+        excerpt: randomexcerpt,
+        user_id: id
+      })
+      
+    }
+
   process.exit(0);
 };
 
