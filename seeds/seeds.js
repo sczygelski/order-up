c28bcc5813095f68cefddfb671d1489cdceadea8
Sczygelski <avamariescz@gmail.com>
2022-09-05 19:55:46 -0500

    adding seeds

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -3,19 +3,24 @@ import { faker } from "@faker-js/faker";
 
 const { User } = require("../models");
 
-const randomName = faker.name.fullName(); // Rowan Nikolaus
+const randomUsername = faker.name.lastName(); // Nikolaus
 const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+const setpassword = "password"
+
+
 
 const seedDatabase = async () => {
+  let generatedusers = []
   await sequelize.sync({ force: true });
 
     for (let i = 0; i < 10; i++) {
-        User.create(
-            {username: }
-        )
-        
+        User.create({
+          "id": id,
+          "username": randomUsername,
+          "email": randomEmail,
+          "password": setpassword
+        })
     }
-
   process.exit(0);
 };
 
