4e4ffc21882bfc42a8aa861549b98c6fad0319b7
Kal O <koestreicher4@gmail.com>
2022-09-07 17:23:26 -0500

    route updates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,23 +31,6 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/signup', (req, res) => {
-  // if (req.session.loggedIn) {
-  //   res.redirect('/');
-  //   return;
-  // }
-  res.render('signup');
-});
-
-
-router.get('/dashboard', (req, res) => {
-  // if (req.session.loggedIn) {
-  //   res.redirect('/');
-  //   return;
-  // }
-  res.render('dashboard');
-});
-
 
 router.get('/add-review', (req, res) => {
   // if (req.session.loggedIn) {
