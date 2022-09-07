3ef942563770b3f363a48a74efce576f1551720f
Jonathan Thompson <jathompson399@gmail.com>
2022-09-07 00:21:47 -0500

    trying get routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,42 +30,53 @@ router.get('/', (req, res) => {
     });
 });
 
-// router.get('/api/reviews/:id', (req, res) => {
-//   Review.findOne({
-//     where: {
-//       id: req.params.id
-//     },
-//     attributes: ['id', 'address', 'review_content', 'created_at'],
-//     // include: [
-//     //   {
-//     //     model: User,
-//     //     attributes: ['user_id']
-//     //   }
-//     // ]
-//   })
-//     .then(dbReviewData => {
-//       if (!dbReviewData) {
-//         res.status(404).json({ message: 'No post found with this id.' });
-//         return;
-//       }
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
-
 router.get('/api/reviews/:id', (req, res) => {
-  const post = {
-    id: 16,
-    address: 'https://handlebarsjs.com/guide/',
-    review_content: 'Handlebars Docs',
-    created_at: new Date(),
-  };
+  Review.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'address',
+      'review_content',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
 
-  res.render('view-reviews', { post });
+      // serialize the data
+      const post = dbPostData.get({ plain: false });
+
+      // pass data to template
+      res.render('view-reviews', { post });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
+// router.get('/api/reviews/:id', (req, res) => {
+//   const post = {
+//     id: 16,
+//     address: 'https://handlebarsjs.com/guide/',
+//     review_content: 'Handlebars Docs',
+//     created_at: new Date(),
+//   };
+
+// res.render('view-reviews', { post });
+// });
 
 
 router.get('/login', (req, res) => {
